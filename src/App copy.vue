e67dfecf5a4e07ebc82cf791aeb712cf2b61779f
HongRu <nic096315649463@gmail.com>
2025-02-03 11:17:44 +0800

    [feat] 商品展示功能、模組化

---
+++ src/App copy.vue
@@ -0,0 +1,23 @@
+<script setup>
+// 引入 Default.vue 作為預設佈局元件
+import DefaultLayout from "./layouts/Default.vue";
+import cards from "./components/BTCard.vue";
+</script>
+
+<template>
+    <!-- 使用 Vuetify 的 v-app 組件作為應用程式的根元素 -->
+    <v-app id="app">
+        <!-- 在 v-app 內部渲染 DefaultLayout 元件，將 DefaultLayout 定義的佈局套用到整個應用程式中。 -->
+        <!-- <DefaultLayout /> -->
+        <cards></cards>
+    </v-app>
+</template>
+
+<style>
+#app {
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container border py-3 my-3">
+        <div class="row">
+            <div class="col-lg-4" v-for="(item, index) in categoryArray" :key="index">
+
+                <div class="card my-2" :data-bs-toggle="'modal'" :data-bs-target="'#modal-' + index">
+                    <div class="card-body">
+                        <h2 class="card-text d-flex justify-content-center">{{ item.eventName }}</h2>
+                        <img src="/imgs/noImage.png" class="card-img-top" alt="應該要顯示封面" :title="item.eventName">
+                        <p class="card-text d-flex justify-content-center fs-3">
+                            <rating :rating="item.ratings" />
+                        </p>
+                    </div>
+                </div>
+
+                <div class="modal fade" :id="'modal-' + index" tabindex="-1" :aria-labelledby="'modalLabel-' + index"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" :id="'modalLabel-' + index">{{ item.eventName }}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>{{ item.eventDescription }}</p>
+                                <div class="d-flex justify-content-between">
+                                    <p>聯絡電話: {{ item.contactInfo }}</p>
+                                    <p>售價: {{ item.price }}</p>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button class="btn btn-primary" @click="show(item)">加到購物車</button>
+                                <button class="btn btn-primary" @click="show(item)">立即結帳</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Rating from './Rating.vue';
+const show = (item) => {
+    alert(JSON.stringify(item.eventName));
+}
+defineProps({
+    categoryArray: Array
+});
+</script>
