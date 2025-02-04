9eb3cd12f383d24589d67d11b1ae0cf1a95cb0fd
theoch3n <theoch3n@gmail.com>
2025-02-04 15:38:25 +0800

    [chore] 新增配置，讓 @ 作為 src 目錄的別名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    // 使用 @ 作為 src 目錄的別名
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+});
