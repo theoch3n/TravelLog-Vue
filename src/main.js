d892082560116cdb13c907118cd582597a05b85f
Wen zhe <wes55309@gmail.com>
2025-02-19 14:38:34 +0800

    [fix] 1. 修正印堂發黑 2. 修正下巴脫臼

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
-// 導入必需的庫與全局樣式
 import { createApp } from "vue";
+import "./style.css"; // 全局樣式
+
 import App from "./App.vue";
-import router from "./router/router"; // 或 "./router" 根據你的實際路徑
-import { createVuetify } from "vuetify";
+import router from "./router/router"; // 引入路由配置
+
+// 引入 Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";//vuex 類似於bootstrap 提供現有樣式及元件
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import "@mdi/font/css/materialdesignicons.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import "./style.css";
-
-// 建立 Vuetify 實例
+import "@mdi/font/css/materialdesignicons.css"; // 引入 Material Design Icons
+// 創建 Vuetify 物件，並註冊所有組件與指令
 const vuetify = createVuetify({
     components,
     directives,
@@ -21,29 +19,13 @@ const vuetify = createVuetify({
     },
 });
 
-// 建立 Pinia 實例
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
+// 引入 Pinia
+import { createPinia } from "pinia";
+const pinia = createPinia(); // 創建 Pinia 物件
 
-// 建立 Vue 應用並掛載
-const app = createApp(App);
-app.use(router);
-app.use(vuetify);
-app.use(pinia);
-app.mount("#app");
-
-// 配置 axios（這部分可以放在單獨的檔案中，如 api.js）
+// 引入 axios
 import axios from "axios";
 axios.defaults.baseURL = "https://localhost:7092"; // API 伺服器 URL
 
-// 每次發送請求時自動附加最新 token
-axios.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+// 創建 Vue 應用程式，並使用路由、Vuetify、Pinia
+createApp(App).use(router).use(vuetify).use(pinia).mount("#app");
